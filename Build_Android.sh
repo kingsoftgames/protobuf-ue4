b30778176fd5a5a64a3c5eb4abd46a1f6e0c1f80
yanzhubiao <yanzhubiao@163.com>
2018-12-14 17:08:06 +0800

    Reformatting shell provisioners (#2)

--- Build_Android.sh
+++ Build_Android.sh
@@ -1,76 +1,76 @@
 #!/bin/bash
 
-if [ -z "$PROTOBUF_UE4_VERSION" ]; then
-    echo "PROTOBUF_UE4_VERSION is not set, exit."
-    exit 1
+if [[ -z "${PROTOBUF_UE4_VERSION}" ]]; then
+  echo "PROTOBUF_UE4_VERSION is not set, exit."
+  exit 1
 else
-    echo "PROTOBUF_UE4_VERSION: $PROTOBUF_UE4_VERSION"
+  echo "PROTOBUF_UE4_VERSION: ${PROTOBUF_UE4_VERSION}"
 fi
 
-if [ -z "$PROTOBUF_UE4_PREFIX" ]; then
-    echo "PROTOBUF_UE4_PREFIX is not set, exit."
-    exit 1
+if [[ -z "${PROTOBUF_UE4_PREFIX}" ]]; then
+  echo "PROTOBUF_UE4_PREFIX is not set, exit."
+  exit 1
 else
-    echo "PROTOBUF_UE4_PREFIX: $PROTOBUF_UE4_PREFIX"
+  echo "PROTOBUF_UE4_PREFIX: ${PROTOBUF_UE4_PREFIX}"
 fi
 
-if [ -z "$NDKROOT" ]; then
-    echo "NDKROOT is not set, exit."
-    exit 1
+if [[ -z "${NDKROOT}" ]]; then
+  echo "NDKROOT is not set, exit."
+  exit 1
 else
-    echo "NDKROOT: $NDKROOT"
+  echo "NDKROOT: ${NDKROOT}"
 fi
 
-PROTOBUF_URL=https://github.com/google/protobuf/releases/download/v$PROTOBUF_UE4_VERSION/protobuf-cpp-$PROTOBUF_UE4_VERSION.tar.gz
-PROTOBUF_DIR=protobuf-$PROTOBUF_UE4_VERSION
-PROTOBUF_TAR=$PROTOBUF_DIR.tar.gz
+readonly PROTOBUF_URL=https://github.com/google/protobuf/releases/download/v${PROTOBUF_UE4_VERSION}/protobuf-cpp-${PROTOBUF_UE4_VERSION}.tar.gz
+readonly PROTOBUF_DIR=protobuf-${PROTOBUF_UE4_VERSION}
+readonly PROTOBUF_TAR=${PROTOBUF_DIR}.tar.gz
 
-rm -rf $PROTOBUF_DIR
+rm -rf ${PROTOBUF_DIR}
 
-wget -q -O $PROTOBUF_TAR $PROTOBUF_URL
+wget -q -O ${PROTOBUF_TAR} ${PROTOBUF_URL}
 
-tar zxf $PROTOBUF_TAR
-cd $PROTOBUF_DIR
+tar zxf ${PROTOBUF_TAR}
+cd ${PROTOBUF_DIR}
 
-rm -rf $PROTOBUF_UE4_PREFIX
-mkdir -p $PROTOBUF_UE4_PREFIX
+rm -rf ${PROTOBUF_UE4_PREFIX}
+mkdir -p ${PROTOBUF_UE4_PREFIX}
 
 # android-19 means Android 4.4
-export SYSROOT=$NDKROOT/platforms/android-19/arch-arm
-export TOOLCHAIN=$NDKROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin
+export SYSROOT=${NDKROOT}/platforms/android-19/arch-arm
+export TOOLCHAIN=${NDKROOT}/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin
 
 # build tools
-export CC="$TOOLCHAIN/arm-linux-androideabi-gcc --sysroot=$SYSROOT"
-export CXX="$TOOLCHAIN/arm-linux-androideabi-g++ --sysroot=$SYSROOT"
-export AR=$TOOLCHAIN/arm-linux-androideabi-ar
-export LD=$TOOLCHAIN/arm-linux-androideabi-ld
-export RANLIB=$TOOLCHAIN/arm-linux-androideabi-ranlib
-export STRIP=$TOOLCHAIN/arm-linux-androideabi-strip
-export READELF=$TOOLCHAIN/arm-linux-androideabi-readelf
-export CXXSTL=$NDKROOT/sources/cxx-stl/gnu-libstdc++/4.9
-export LIBS="-llog $CXXSTL/libs/armeabi-v7a/libgnustl_static.a"
+export CC="${TOOLCHAIN}/arm-linux-androideabi-gcc --sysroot=${SYSROOT}"
+export CXX="${TOOLCHAIN}/arm-linux-androideabi-g++ --sysroot=${SYSROOT}"
+export AR=${TOOLCHAIN}/arm-linux-androideabi-ar
+export LD=${TOOLCHAIN}/arm-linux-androideabi-ld
+export RANLIB=${TOOLCHAIN}/arm-linux-androideabi-ranlib
+export STRIP=${TOOLCHAIN}/arm-linux-androideabi-strip
+export READELF=${TOOLCHAIN}/arm-linux-androideabi-readelf
+export CXXSTL=${NDKROOT}/sources/cxx-stl/gnu-libstdc++/4.9
+export LIBS="-llog ${CXXSTL}/libs/armeabi-v7a/libgnustl_static.a"
 
 ./autogen.sh
 
-./configure --prefix=$PROTOBUF_UE4_PREFIX/build \
-    --host=arm-linux-androideabi \
-    --with-sysroot=$SYSROOT \
-    --disable-shared \
-    --disable-debug \
-    --disable-dependency-tracking \
-    --enable-cross-compile \
-    --with-protoc=protoc \
-    CFLAGS="-march=armv7-a" \
-    CXXFLAGS="$CFLAGS -I$CXXSTL/include -I$CXXSTL/libs/armeabi-v7a/include"
+./configure --prefix=${PROTOBUF_UE4_PREFIX}/build         \
+  --host=arm-linux-androideabi                            \
+  --with-sysroot=${SYSROOT}                               \
+  --disable-shared                                        \
+  --disable-debug                                         \
+  --disable-dependency-tracking                           \
+  --enable-cross-compile                                  \
+  --with-protoc=protoc                                    \
+  CFLAGS="-march=armv7-a"                                 \
+  CXXFLAGS="${CFLAGS} -I${CXXSTL}/include -I${CXXSTL}/libs/armeabi-v7a/include"
 
 make -j$(nproc)
 make install
 
-rm -rf $PROTOBUF_UE4_PREFIX/android
-mkdir -p $PROTOBUF_UE4_PREFIX/android/lib
+rm -rf ${PROTOBUF_UE4_PREFIX}/android
+mkdir -p ${PROTOBUF_UE4_PREFIX}/android/lib
 
-mv $PROTOBUF_UE4_PREFIX/build/lib/libprotobuf.a $PROTOBUF_UE4_PREFIX/android/lib/libprotobuf.a
+mv ${PROTOBUF_UE4_PREFIX}/build/lib/libprotobuf.a ${PROTOBUF_UE4_PREFIX}/android/lib/libprotobuf.a
 
-rm -rf $PROTOBUF_UE4_PREFIX/build
+rm -rf ${PROTOBUF_UE4_PREFIX}/build
 
-objdump -h $PROTOBUF_UE4_PREFIX/android/lib/libprotobuf.a | head -n 25
+objdump -h ${PROTOBUF_UE4_PREFIX}/android/lib/libprotobuf.a | head -n 25
--- Build_IOS.sh
+++ Build_IOS.sh
@@ -1,50 +1,49 @@
 #!/bin/bash
 
-CORE_COUNT=$(sysctl -n machdep.cpu.core_count)
-
-if [ -z "${PROTOBUF_UE4_VERSION}" ]; then
+if [[ -z "${PROTOBUF_UE4_VERSION}" ]]; then
     echo "PROTOBUF_UE4_VERSION is not set, exit."
     exit 1
 else
-    echo "PROTOBUF_UE4_VERSION: $PROTOBUF_UE4_VERSION"
+    echo "PROTOBUF_UE4_VERSION: ${PROTOBUF_UE4_VERSION}"
 fi
 
-if [ -z "${PROTOBUF_UE4_PREFIX}" ]; then
+if [[ -z "${PROTOBUF_UE4_PREFIX}" ]]; then
     echo "PROTOBUF_UE4_PREFIX is not set, exit."
     exit 1
 else
-    echo "PROTOBUF_UE4_PREFIX: $PROTOBUF_UE4_PREFIX"
+    echo "PROTOBUF_UE4_PREFIX: ${PROTOBUF_UE4_PREFIX}"
 fi
 
-if [ -z "${PROTOBUF_UE4_IOS_DEPLOYMENT_TARGET}" ]; then
+if [[ -z "${PROTOBUF_UE4_IOS_DEPLOYMENT_TARGET}" ]]; then
     echo "PROTOBUF_UE4_IOS_DEPLOYMENT_TARGET is not set, exit."
     exit 1
 else
-    echo "PROTOBUF_UE4_IOS_DEPLOYMENT_TARGET: $PROTOBUF_UE4_IOS_DEPLOYMENT_TARGET"
+    echo "PROTOBUF_UE4_IOS_DEPLOYMENT_TARGET: ${PROTOBUF_UE4_IOS_DEPLOYMENT_TARGET}"
 fi
 
-rm -rf $PROTOBUF_UE4_PREFIX
-mkdir -p $PROTOBUF_UE4_PREFIX
+readonly CORE_COUNT=$(sysctl -n machdep.cpu.core_count)
+readonly PROTOBUF_URL=https://github.com/google/protobuf/releases/download/v${PROTOBUF_UE4_VERSION}/protobuf-cpp-${PROTOBUF_UE4_VERSION}.tar.gz
+readonly PROTOBUF_DIR=protobuf-${PROTOBUF_UE4_VERSION}
+readonly PROTOBUF_TAR=${PROTOBUF_DIR}.tar.gz
 
-PROTOBUF_URL=https://github.com/google/protobuf/releases/download/v$PROTOBUF_UE4_VERSION/protobuf-cpp-$PROTOBUF_UE4_VERSION.tar.gz
-PROTOBUF_DIR=protobuf-$PROTOBUF_UE4_VERSION
-PROTOBUF_TAR=$PROTOBUF_DIR.tar.gz
+rm -rf ${PROTOBUF_UE4_PREFIX}
+mkdir -p ${PROTOBUF_UE4_PREFIX}
 
-rm -rf $PROTOBUF_DIR
-wget -q -O $PROTOBUF_TAR $PROTOBUF_URL
+rm -rf ${PROTOBUF_DIR}
+wget -q -O ${PROTOBUF_TAR} ${PROTOBUF_URL}
 
-tar zxf $PROTOBUF_TAR
-cd $PROTOBUF_DIR/cmake
+tar zxf ${PROTOBUF_TAR}
+cd ${PROTOBUF_DIR}/cmake
 
-cmake -DCMAKE_INSTALL_PREFIX=$PROTOBUF_UE4_PREFIX/ios . -G "Xcode"
+cmake -DCMAKE_INSTALL_PREFIX=${PROTOBUF_UE4_PREFIX}/ios . -G "Xcode"
 xcodebuild -project protobuf.xcodeproj                               \
   -target libprotobuf                                                \
   -configuration Release                                             \
   -sdk iphoneos                                                      \
   -arch arm64                                                        \
-  IPHONEOS_DEPLOYMENT_TARGET=$PROTOBUF_UE4_IOS_DEPLOYMENT_TARGET     \
+  IPHONEOS_DEPLOYMENT_TARGET=${PROTOBUF_UE4_IOS_DEPLOYMENT_TARGET}   \
   GCC_SYMBOLS_PRIVATE_EXTERN=YES                                     \
-  -jobs $CORE_COUNT                                                  \
+  -jobs ${CORE_COUNT}                                                \
   build
 xcodebuild -target install build
 
