d2d027965e1bc3682cc7d1338b71a9960d9bdcef
闫竹彪 <yanzhubiao@kingsoft.com>
2019-10-22 15:04:32 +0800

    Using version 3.9.2.

---
+++ Fix-3.9.2.bat
@@ -0,0 +1,23 @@
+@echo off
+
+cd %PROTOBUF_DIR%\src\google\protobuf
+
+REM see: https://medium.com/@0xflarion/using-ue4-w-google-protocol-buffers-ae7cab820d84 step2
+REM Error C4647	behavior change: __is_pod(google::protobuf::internal::AuxillaryParseTableField) has different value in previous versions \
+REM Pirates	C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.14.26428\INCLUDE\type_traits
+powershell -Command "$text=Get-Content generated_message_table_driven.h ;" ^
+                    "$text[209]='// ' ;" ^
+                    "$text| Set-Content generated_message_table_driven.h"
+
+REM disable warning: 4946
+powershell -Command "$text=Get-Content type.pb.h ;" ^
+                    "$text[0]='#ifdef _MSC_VER' ;" ^
+                    "$text[1]='#pragma warning (disable: 4946)' ;" ^
+                    "$text[2]='#endif' ;" ^
+                    "$text| Set-Content type.pb.h"
+
+pushd stubs
+    powershell -Command "$text=Get-Content port.h ;" ^
+                        "$text[205]='#elif defined(GOOGLE_PROTOBUF_USE_UNALIGNED)' ;" ^
+                        "$text| Set-Content port.h"
+popd
